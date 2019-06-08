9c478a4468f39be5b1893d73b562098a45665f3d
Brandon U <unzueta.brandon@gmai.com>
2019-06-07 21:52:00 -0700

    working on some arrays

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var guessWord = ["trust", "mirror", "lawsuit","darkness", "secret","time", "coffin",];
+var riddles = ["You can have me but cannot hold me, gain me and quickly lose me, if treated with care I can be great, and if betrayed I will break. What am I?",
+"Hit me hard and I will crack. But you'll never stop me from staring back. What am I?",
+"What is it that no man wants to have but no man wants to lose?",
+"The more there is the less you see. What could I be?",
+"If you know me, you'll want to share me. But if you share me, I'll be gone. What am I?",
+"I never stop, I control your life but without me you wouldn't go anywhere. What am I?",
+"The man who invented it doesn't want it, the man who bought it doesn't need it, and the man who needs it doesn't know. What is it?"];
+
+var guess;
+guess.onkeyup = function () {
+    
+}
